023060fd6f64a41751bcd67d5f1ad793b32a7f24
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-21 11:01:32 -0800

    modified DayList file

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,7 @@ export default function DayList(props) {
   const daylistarr = props.days;
   const output = daylistarr.map(day => (
     <DayListItem
+    key = {day.id}
     name = {day.name}
     spots = {day.spots}
     selected = {day.name === props.day}
