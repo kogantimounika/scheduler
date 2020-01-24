683224e529d80941521be1c8c4fbc9e48cf0f06c
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-23 20:39:43 -0800

    added save option to book an appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,30 @@ import Form from "components/Appointment/Form"
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Status from "components/Appointment/Status";
 import useVisualMode  from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+
 export default function Appointment(props) {
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
+ 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    return interview;
+  }
+
   return (
   <article className="appointment">
     <Header time={props.time} />
@@ -25,10 +38,16 @@ export default function Appointment(props) {
     />
     )}
     {mode === CREATE && ( <Form
-    interviewers = {[]}
+    interviewers = {props.interviewersDay}
+    onSave={(name, interviewer) => {
+      transition(SAVING);
+      props.bookInterview(props.id,save(name, interviewer))
+      .then(() => transition(SHOW))
+    }}
     onCancel={() => back()}
     />
     )} 
+    {mode === SAVING && <Status message="Saving" />}
   </article>
   )
 }
