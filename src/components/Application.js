84cdc515632f96a89000b0029a57488d066dfc7d
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-24 16:26:10 -0800

    seperated all the functions and setDay to custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,71 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DayList from "../components/DayList";
 import Appointment from "../components/Appointment";
 import axios from "axios";
 import "components/Application.scss";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
+import useApplicationData  from "hooks/useApplicationData"
+
 
 
 export default function Application(props) {
 
-  const setDay = day => setState({ ...state, day });
-  const setInterviwers = interviewersDay => setState({ ...state, interviewersDay});
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  });
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`, { interview })
-    .then(() => setState({...state, appointments}))
-}
-  
-  function cancelInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`, { interview: null})
-    .then(() => setState({...state, appointments}))
-  }
-
-
-  useEffect(() => {
-    const days = axios.get("http://localhost:8001/api/days");
-    const appointments = axios.get("http://localhost:8001/api/appointments");
-    const interviewers = axios.get("http://localhost:8001/api/interviewers");
-    Promise.all([ days, appointments, interviewers]).then(([responseDays, responseAppointments, responseInterviewers]) => {
-      console.log("days", responseDays);
-      console.log("appointments", responseAppointments);
-      console.log("interviewers", interviewers);
-        
-      const result = getInterviewersForDay({
-        interviewers: responseInterviewers.data,
-        days: responseDays.data
-      }, state.day)
-
-      setState(prev => ({
-        ...prev,
-        days: responseDays.data,
-        appointments: responseAppointments.data,
-        interviewers: responseInterviewers.data,
-        interviewersDay: result
-      }))
-    });
-  }, []);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const appointments = getAppointmentsForDay(state, state.day);
   const schedule = appointments.map((appointment) => {
@@ -83,15 +33,9 @@ export default function Application(props) {
     );
   });
 
-  useEffect(() => {
-    const result = getInterviewersForDay(state, state.day)
-    setInterviwers(result);
-  }, [state.day])
-
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
         className="sidebar--centered"
         src="images/logo.png"
