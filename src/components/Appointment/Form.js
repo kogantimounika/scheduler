5a0a7885c01aac27432c6b41eb8ad79209d77159
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-28 13:22:10 -0800

    Added validate function to the form to enter the student name field

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,56 +2,59 @@ import React, {useState} from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-
 export default function Form(props) {
   const { interviewers, onCancel, onSave } = props;
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setName("");
     setInterviewer(null);
   }
+
   function cancel() {
     reset();
     onCancel();
   }
+
   function save() {
     onSave(name, interviewer);
   }
 
-
-  return (
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    onSave(name, interviewer);
+  }
+  return(
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
       <form autoComplete="off">
         <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        value={name}
-        placeholder="Enter Student Name"
-        onChange={(event) => setName(event.target.value)}
-        data-testid="student-name-input"
-        // onSave = {(event) => setName(event.target.value)}
-        /*
-          This must be a controlled component
-        */
-  
+          className="appointment__create-input text--semi-bold"
+          name="name"
+          type="text"
+          value={name}
+          placeholder="Enter Student Name"
+          onChange = {(event)=>setName(event.target.value)}
+          data-testid="student-name-input"
         />
+        <section className="appointment__validation">{error}</section>
       </form>
-      <InterviewerList 
-      interviewers={interviewers} 
-      value={interviewer} 
+      <InterviewerList interviewers={interviewers} value={interviewer}
       onChange={setInterviewer} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-      <Button confirm onClick={save} >Save</Button>
       <Button onClick={cancel} danger>Cancel</Button>
-      
+      {/* <Button onClick={save} confirm>Save</Button> */}
+      <Button onClick={()=>validate()} confirm>Save</Button>
       </section>
     </section>
   </main>
-  )
-}
+)};
+
