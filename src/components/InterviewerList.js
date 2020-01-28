bba7abc032a17f5f00f73a72472ba76da7ded699
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-28 13:25:05 -0800

    imported proptypes to the interview list file

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,35 +1,13 @@
-// import React from "react";
-// import InterviewerListItem from "./InterviewerListItem"
-// import "components/InterviewerList.scss"
-
-
-// export default function InterviewerList(props) {
-//   //  const { interviewers, setInterviewer, interviewer } = props;
-//    const interviewers = props.interviewers.map(interviewer => {
-//     return (
-//       <InterviewerListItem
-//         key={interviewer.id}
-//         name={interviewer.name}
-//         avatar={interviewer.avatar}
-//         selected={interviewer.id === props.value}
-//         setInterviewer={event => props.setInterviewer(interviewer.id)}
-//       />
-//     );
-//   });
-
-//     return (
-//       <section className="interviewers">
-//       <h4 className="interviewers__header text--light">Interviewer</h4>
-//       <ul className="interviewers__list">{interviewers}</ul>
-//       </section>
-//     )
-// }
-
-
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem"
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
+
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
   const { interviewers, onChange, value } = props;
   const interviewerList = interviewers.map(({
     id,
@@ -52,4 +30,5 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
+
 };
