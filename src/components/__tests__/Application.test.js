615b6942b98e6769d3c951d41071013d9a4e7d85
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-28 13:22:39 -0800

    Added test cases to the form

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,9 +3,17 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 
 import Application from "components/Application";
+import Appointment from "components/Application";
+
 
 afterEach(cleanup);
 
 it("renders without crashing", () => {
   render(<Application />);
+});
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
