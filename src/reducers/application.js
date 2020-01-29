5bb38163924d15e5a8c00846e7943a56e9634d13
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-29 15:41:24 -0800

    moved the code from useApplicationData to this file and used the reducer

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,67 +1,12 @@
-// const SET_DAY = "SET_DAY";
-// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-// const SET_INTERVIEW = "SET_INTERVIEW";
-// const SET_INTERVIEWERS = "SET_INTERVIEWERS";
-// const UPDATE_SPOTS = "UPDATE_SPOTS";
-
-// function reducer(state, action) {
-//   switch (action.type){
-//     case SET_DAY:
-//       return {...state, day: action.value};
-//     case SET_APPLICATION_DATA:
-//       return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers, interviewersDay : action.interviewersDay};
-//     case SET_INTERVIEW:
-//       const updated = {
-//         ...state.appointments[action.id],
-//         interview: action.interview
-//       };
-//       const updateAppointments = {
-//       ...state.appointments,
-//       [action.id]: updated
-//       };
-//       return {...state,
-//         appointments: updateAppointments};
-//     case SET_INTERVIEWERS:
-//       const result = getInterviewersForDay(state, state.day)
-//       return{...state, interviewersDay: result}
-//     case UPDATE_SPOTS:
-//     const dayId = findDayByAppointment(action.id, state);
-//     const aptId = state.days[dayId].appointments;
-//       let newSpots = 0;
-//         for (let i = 0; i < aptId.length; i++) {
-//           if (!state.appointments[aptId[i]].interview) {
-//             newSpots += 1;
-//           }
-//         }
-//       return {...state, 
-//       days: state.days.map((item, index) => {
-//         if (index !== dayId) {
-//           return item
-//         } else {
-//           return {
-//             ...item,
-//             spots: newSpots
-//           }
-//         }
-//       })}
-//     default:
-//       throw new Error(`Tried to reduce with unsupported action type: ${action.type}`
-//       ); 
-//   }
-// }
-
-
-
-
 import {
-  // getInterviewersForDay,
+  getInterviewersForDay,
   findDayByAppointment
 } from "../helpers/selectors";
 
 export const SET_DAY = "SET_DAY";
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_INTERVIEW = "SET_INTERVIEW";
-// export const SET_INTERVIEWERS = "SET_INTERVIEWERS";
+export const SET_INTERVIEWERS = "SET_INTERVIEWERS";
 export const UPDATE_SPOTS = "UPDATE_SPOTS";
 
 export default function reducer(state, action) {
@@ -88,9 +33,9 @@ export default function reducer(state, action) {
         [action.id]: updated
       };
       return { ...state, appointments: updateAppointments };
-    // case SET_INTERVIEWERS:
-    //   const result = getInterviewersForDay(state, state.day);
-    //   return { ...state, interviewersDay: result };
+    case SET_INTERVIEWERS:
+      const result = getInterviewersForDay(state, state.day);
+      return { ...state, interviewersDay: result };
     case UPDATE_SPOTS:
       const dayId = findDayByAppointment(action.id, state);
       const aptIds = state.days[dayId].appointments;
