46377634d0c127f03a2d817c29cbcbaa80a18df7
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-28 21:04:12 -0800

    added integration testing test cases to application file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,16 @@
 
 import React from "react";
-import { render , cleanup , waitForElement , fireEvent } from "@testing-library/react";
+import { 
+  render, 
+  cleanup,
+  waitForElement,
+  fireEvent,
+  getByText,
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Application from "components/Application";
 
@@ -14,4 +24,26 @@ describe("Appointment", () => {
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
   });
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
+  });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+    describe("Form", () => {}) 
+  })
 });
