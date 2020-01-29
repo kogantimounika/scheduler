33514780ace18eced4c4bea6db3bf246ddf97444
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-29 15:01:59 -0800

    Added the data test id prop to the onclick event

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,7 +19,6 @@ export default function DayListItem(props) {
     }
   }
   return (
-    // <li className= { displayClass } onClick={() => props.setDay(props.name)}>
     <li className= { displayClass } onClick={() => props.setDay(props.name)} data-testid="day">
     <h2 className="text--regular">{ props.name }</h2>
     <h3 className="text--light">{ formatSpots() }</h3>
