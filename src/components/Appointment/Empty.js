51e57381c9e34ec51eb7b1c997c45fbcabfe6bcd
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-01-29 18:28:27 -0800

    removed the unused code

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -2,20 +2,17 @@ import React from "react"
 
 import "components/Appointment/styles.scss";
 
-
 export default function Empty(props) {
   return(
-    <main className="appointment__add">
-  <img
+  <main className="appointment__add">
+    <img
     className="appointment__add-button"
     src="images/add.png"
     alt="Add"
     onClick={props.onAdd}
-  />
-</main>
-
+    />
+  </main>
   )
-
 }
 
 
